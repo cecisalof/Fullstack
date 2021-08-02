98ee70bbb644516214fea5b0b6645cc183947c04
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-08-02 13:00:39 +0200

    Refactoring of Part&Content components to match the request of exercise 1.2 step 2

--- part1/src/App.js
+++ part1/src/App.js
@@ -18,12 +18,10 @@ const Header= (props) => {
  }
 
 const Part = (props) => {
-  console.log(props);
+  // console.log(props);
 return (
   <>
-        <p> {props.part1}: {props.exercises1}</p>
-        <p> {props.part2}: {props.exercises2}</p>
-        <p> {props.part3}: {props.exercises3}</p>
+         <p> {props.part1} {props.part2} {props.part3}: {props.exercises1} {props.exercises2} {props.exercises3} </p>
   </>
 )
 
@@ -33,7 +31,10 @@ const Content = (props) => {
   
     return (
       <>
-        <Part part1={props.part1} part2={props.part2} part3={props.part3} exercises1={props.exercises1} exercises2={props.exercises2} exercises3={props.exercises3}/>
+        {/* <Part part1={props.part1} part2={props.part2} part3={props.part3} exercises1={props.exercises1} exercises2={props.exercises2} exercises3={props.exercises3}/> */}
+        <Part part1={props.part1} exercises1={props.exercises1} />
+        <Part part2={props.part2} exercises2={props.exercises2} />
+        <Part part3={props.part3} exercises3={props.exercises3} />
      </>
    )
  }

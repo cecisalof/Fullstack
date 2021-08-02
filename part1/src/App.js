a987171befb7004a0acec9b56eb4d3cc9fbe0d3a
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-08-02 14:08:16 +0200

    An approach to destructuring assigment. New way to refactored code for exercise 1.2 second part

--- part1/src/App.js
+++ part1/src/App.js
@@ -18,7 +18,7 @@ const Header= (props) => {
  }
 
 const Part = (props) => {
-  // console.log(props);
+// console.log("From Part component", props);
 return (
   <>
          <p> {props.part1} {props.part2} {props.part3}: {props.exercises1} {props.exercises2} {props.exercises3} </p>
@@ -28,12 +28,17 @@ return (
 }
 
 const Content = (props) => {
-  
+  console.log("From Content component", props);
+  const partOfCourse = [props.part1, props.part2, props.part3];
+  const numberOfExercises = [props.exercises1, props.exercises2, props.exercises3];
+
+  const [one, two, three ] = partOfCourse; // practicing destructuring assigment 
+
     return (
       <>
-        <Part part1={props.part1} exercises1={props.exercises1} />
-        <Part part2={props.part2} exercises2={props.exercises2} />
-        <Part part3={props.part3} exercises3={props.exercises3} />
+        <Part part1={one} exercises1={numberOfExercises[0]} />
+        <Part part2={two} exercises2={numberOfExercises[1]} />
+        <Part part3={three} exercises3={numberOfExercises[2]} />
      </>
    )
  }
@@ -61,7 +66,9 @@ const App = () => {
   return (
     <div>
       <Header course ={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1}
+               part2={part2} exercises2={exercises2}
+               part3={part3} exercises3={exercises3} />
       <Total count ={exercises1 + exercises2 + exercises3}/>
     </div>
   )

bc8982938a69a23df88dcd2fd4a072b636ee1a48
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-08-04 13:56:53 +0200

    Step4: collecting data into an array & refactoring the whole application

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,7 @@
 import React from 'react'
 
-/*
-Refactor the code in 3 componentS:
-Header: renders the name of the course
-Content: renders the parts and their number of exercises
-Total: renders the total number of exercises.
-
-All the data remain in App.js file so the data will be passed to the components as props
-*/
- 
 const Header= (props) => {
+  //console.log("From Header component", props)
    return (
      <>
        <h1>{props.course}</h1>
@@ -18,7 +10,7 @@ const Header= (props) => {
  }
 
 const Part = (props) => {
-  console.log("From Part component", props);
+  //console.log("From Part component", props);
   return (
     <>
           <p> {props.part1} {props.part2} {props.part3}: {props.exercises1} {props.exercises2} {props.exercises3} </p>
@@ -28,27 +20,30 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  console.log("From Content component", props);
- 
-  const partOfCourse = [props.part1, props.part2, props.part3];
-  const numberOfExercises = [props.exercises1, props.exercises2, props.exercises3];
-
-  const [one, two, three ] = partOfCourse; // practicing destructuring assigment 
+  //console.log("From Content component", props.parts[1].name);
+  
+  const partOfCourse = [props.parts[0].name, props.parts[1].name, props.parts[2].name];
+  const numberOfExercises = [props.parts[0].exercises, props.parts[1].exercises, props.parts[2].exercises];
+  //console.log(partOfCourse);
+  //console.log(numberOfExercises);
 
     return (
       <>
-        <Part part1={one} exercises1={numberOfExercises[0]} />
-        <Part part2={two} exercises2={numberOfExercises[1]} />
-        <Part part3={three} exercises3={numberOfExercises[2]} />
+        <Part part1={partOfCourse[0]} exercises1={numberOfExercises[0]} /> 
+        <Part part2={partOfCourse[1]} exercises2={numberOfExercises[1]} /> 
+        <Part part3={partOfCourse[2]} exercises3={numberOfExercises[2]} />        
      </>
    )
  }
 
 
  const Total = (props) => {
+   //console.log("From Total component", props.parts[0].exercises)
+   const exercisesSum = (props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises)
+   //console.log(exercisesSum);
    return (
      <>
-      <p>Number of exercises: {props.count}</p>
+      <p>Number of exercises: {exercisesSum}</p>
      </>
    )
  }
@@ -57,26 +52,26 @@ const Content = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  //console.log("Accesig data from array", parts[0].name)
   return (
     <div>
       <Header course ={course}/>
-      <Content part1={part1.name} exercises1={part1.exercises}
-               part2={part2.name} exercises2={part2.exercises}
-               part3={part3.name} exercises3={part3.exercises} />
-      <Total count ={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}  />
+      <Total parts={parts}    />
     </div>
   )
 }

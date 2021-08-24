cba3ea7220dee9d4b6b57fb328243278d3842809
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-08-24 13:44:14 +0200

    Cleaning code for unicafe section step 6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,45 +1,31 @@
 import React, {useState} from 'react'
-import './App.css';
 
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}> {text} </button>
   );
 
-const Statistics = ({good, neutral, bad}) => {
-  const total = (good + neutral + bad);
-  const average = (good - bad) / total ; 
-
- 
+const Statistics = ({good, neutral, bad, total, average, percentage}) => {
   return(
     (total !== 0 ?
-    <>
-    <Statistic text='Good:' value={good} />
-    <Statistic text='Neutral:' value={neutral} />
-    <Statistic text='Bad:' value={bad} />
-    <Statistic text='All:' value={good + neutral + bad} />
-    <Statistic text='Average:' value={average} />
-    <Statistic text='Positives:' value={good / total * 100} unit={'%'}/>
-   </>
+    <table>
+      <tbody>
+        <Statistic text='Good:' value={good} />
+        <Statistic text='Neutral:' value={neutral} />
+        <Statistic text='Bad:' value={bad} />
+        <Statistic text='All:' value={total} />
+        <Statistic text='Average:' value={average} />
+        <Statistic text='Positives:' value={percentage} />
+      </tbody>
+   </table>
    : <h5> No feedback given yet! </h5>)
    );
 }
 
 const Statistic = ({text, value, unit}) => ( 
-    // <table>
-      <tbody> 
-      {/* <tbody> tag needs to wrap any <tr> element. The browser will 
-      return an error if this tag is missing. */}
-       {/* <colgroup>
-         <col className='column'></col>
-       </colgroup>  */}
-        <tr>
-          <td className='statistics'> {text} </td> <td className='statistics'> {value}</td>
-          <td className='statistics'> {unit} </td>
-        </tr>
-      </tbody>
-    // </table>
-
-    
+    <tr>
+      <td> {text} </td> 
+      <td> {value}</td>
+    </tr>     
   );
  
 const App = () => {
@@ -47,27 +33,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  // const [total, setTotal] = useState(0)
-  // const [average, setAverage] = useState(0);
+  const total = good + neutral + bad;
+  const average = (good - bad) / total; 
+  const percentage = good / total * 100 + '%';
   
   const handleGoodFeedback = () => {
       setGood(good +1);
-      // setTotal(total +1)
-      // setAverage(average +1);
       console.log('Congratulations! You have recieved a good feedback on ' + new Date())
     }
     
   const handleNeutralFeedback = () => {
-      setNeutral(neutral +1);
-      // setTotal(total +1)
-      // setAverage(average);
+      setNeutral(neutral +1)
       console.log('Neutral customer´s feedback on ' + new Date())
   }
 
   const handleBadFeedback = () => {
       setBad(bad +1);
-      // setTotal(total +1)
-      // setAverage(average -1)
       console.log('Watch out! Bad customer´s feedback on ' + new Date())
   }
 
@@ -82,8 +63,9 @@ const App = () => {
       good={good} 
       neutral={neutral} 
       bad={bad} 
-      // total={total}
-      // average={average} 
+      total={total}
+      average={average} 
+      percentage={percentage}
       />
       
     </>
